b0ad4c15889409933fd0ceeda638df89e1d21b28
KaterynaBogush <kate.bogush89@gmail.com>
2024-11-22 11:38:53 +0200

    add components and router

--- star-wars/src/app/app.routes.ts
+++ star-wars/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {MoviesComponent} from '../movies/movies.component';
+import {MoviesDetailsComponent} from '../movies-details/movies-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MoviesComponent },
+    {path: 'movie/:id', component: MoviesDetailsComponent},
+
+    {path: '**', redirectTo:'', component: MoviesComponent}
+];
